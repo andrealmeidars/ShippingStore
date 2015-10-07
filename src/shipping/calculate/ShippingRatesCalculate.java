5aa71712ff6b285193142dcad51241fc1a055ab0
Andre Almeida <andre.almeida@msn.com>
2015-10-07 08:03:35 -0300

    Modify calculateShippingRates method
    Remove all logic this method and add only one If

--- src/shipping/calculate/ShippingRatesCalculate.java
+++ src/shipping/calculate/ShippingRatesCalculate.java
@@ -1,41 +1,27 @@
 package shipping.calculate;
 
 
-import calculate.shipping.rules.DhlRates;
+
 import calculate.shipping.rules.FedexRates;
-import calculate.shipping.rules.FreeRates;
-import calculate.shipping.rules.ShippingRates;
 import printer.Printer;
 import product.Product;
 
 public class ShippingRatesCalculate {
-    private DhlRates dhlRates;
-    private FedexRates fedexRates;
-    private FreeRates freeRates;
     private Printer printer;
 
 
     public ShippingRatesCalculate(){
-        this.dhlRates = new DhlRates();
-        this.fedexRates = new FedexRates();
-        this.freeRates = new FreeRates();
+
         this.printer = new Printer();
     }
 
 
-    public void calculateShippingRates(Product product){
+    public double calculateShippingRates(Product product, RateType rateType) {
 
-        ShippingRatesCalculate shippingRatesCalculate = new ShippingRatesCalculate();
-
-        double dhlRatesValue = this.dhlRates.calculateRates(product);
-        double fedexRatesValue = this.fedexRates.calculateRates(product);
-        double freeRatesValue = this.freeRates.calculateRates(product);
-
-        printer.print(dhlRates.returnTypeShippping(), dhlRatesValue );
-        printer.print(fedexRates.returnTypeShippping(), fedexRatesValue );
-        printer.print(freeRates.returnTypeShippping(), freeRatesValue );
-
-    }
+        if (rateType.equals(RateType.FEDEX)) {
+            return new FedexRates().calculateRates(product);
+        }
+    
 
 
 
