1b70e6a358d42c886107e29bc73a3f60aea0e8b8
Andre Almeida <andre.almeida@msn.com>
2015-10-07 07:58:31 -0300

    Remove Functions
    Remove functions Sets : setPrince, setFragile and setIsOverWeight

--- src/product/Product.java
+++ src/product/Product.java
@@ -14,20 +14,14 @@ public class Product {
 	public double getPrice() {
 		return price;
 	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
+
 	public boolean isFragile() {
 		return isFragile;
 	}
-	public void setFragile(boolean isFragile) {
-		this.isFragile = isFragile;
-	}
+
 	public boolean isOverWeight() {
 		return isOverWeight;
 	}
-	public void setOverWeight(boolean isOverWeight) {
-		this.isOverWeight = isOverWeight;
-	}
+
 }
 
