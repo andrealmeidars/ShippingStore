42c9b7af84ad89ee426abf1fa0c784f697432c92
Andre Almeida <andre.almeida@msn.com>
2015-10-10 19:40:34 -0300

    Insert and rename method in ShippingRatesCalculateTest
    Insert setup method
    rename calculateIfProductIsFragileAndOverHeigthInDhlRate, calculateIfProductIsFragileAndOverHeigthInFedexRate, and calculateIfProductIsFragileAndOverHeigthInFreeRate

--- test/shipping/calculate/ShippingRatesCalculareTest.java
+++ test/shipping/calculate/ShippingRatesCalculareTest.java
@@ -1,39 +1,37 @@
 package shipping.calculate;
 
+import Interface.ShippingRates;
 import calculate.shipping.rules.RateType;
 import org.junit.Before;
 import org.junit.Test;
 import product.Product;
-import shipping.calculate.ShippingRatesCalculate;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 
 public class ShippingRatesCalculareTest {
     private Product product;
+    ShippingRatesCalculate shippingRatesCalculate;
 
     @Before
     public void setUp() throws Exception {
         product = new Product(2.0, true, true);
+        this.shippingRatesCalculate= new ShippingRatesCalculate();
 
     }
 
     @Test
-    public void ifShippingRatesCalculateReturnCorrectValueWithDhlSipping(){
-        ShippingRatesCalculate shippingRatesCalculate = new ShippingRatesCalculate();
+    public void calculateIfProductIsFragileAndOverHeigthInDhlRate(){
         assertThat(shippingRatesCalculate.calculateShippingRates(product, RateType.DHL), is(500.5));
      }
 
     @Test
-    public void ifShippingRatesCalculateReturnCorrectValueWithFedexSipping(){
-        ShippingRatesCalculate shippingRatesCalculate = new ShippingRatesCalculate();
+    public void calculateIfProductIsFragileAndOverHeigthInFedexRate(){
         assertThat(shippingRatesCalculate.calculateShippingRates(product, RateType.FEDEX), is(300.2));
     }
 
     @Test
-    public void ifShippingRatesCalculateReturnCorrectValueWithFreeSipping(){
-        ShippingRatesCalculate shippingRatesCalculate = new ShippingRatesCalculate();
+    public void calculateIfProductIsFragileAndOverHeigthInFreeRate(){
         assertThat(shippingRatesCalculate.calculateShippingRates(product, RateType.FREE), is(2.0));
     }
 
