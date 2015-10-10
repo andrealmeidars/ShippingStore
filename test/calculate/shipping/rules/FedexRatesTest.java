fc4160028d122d451bf3947b3ff6cf8436d9ca8f
Andre Almeida <andre.almeida@msn.com>
2015-10-10 19:31:31 -0300

    Insert and rename methodo in FedexRatesTest
    Insert setup method
    rename returnTaxFedexValue, calculateIfProductIsFragile and calculateIfProductisOverWeigth,

--- test/calculate/shipping/rules/FedexRatesTest.java
+++ test/calculate/shipping/rules/FedexRatesTest.java
@@ -1,6 +1,7 @@
 package calculate.shipping.rules;
 
 import calculate.shipping.rules.FedexRates;
+import org.junit.Before;
 import product.Product;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
@@ -8,33 +9,43 @@ import static org.junit.Assert.assertThat;
 
 
 public class FedexRatesTest {
+
+    FedexRates fedexRates;
+
+
     private Product productIsFragileValue;
     private Product productIsOverWeigth;
+    private  Product productTaxValue;
 
 
     public FedexRatesTest(){
         this.productIsFragileValue = new Product(2.0, true, false);
         this.productIsOverWeigth = new Product(2.0, false, true);
+        this.productTaxValue = new Product(2.0, false, false);
 
     }
 
+    @Before
+    public void setUp() throws Exception {
+        fedexRates = new FedexRates();
+
+    }
+
+
+
     @Test
-    public void ifCalculateFedexRateValueIsCorrect(){
-        FedexRates fedexRates = new FedexRates();
-        Product productTaxValue = new Product(2.0, false, false);
+    public void returnTaxFedexValue(){
         assertThat(fedexRates.calculateRates(productTaxValue), is(0.2));
     }
 
 
     @Test
     public void calculatesIfProductIsFragile(){
-        FedexRates fedexRates = new FedexRates();
         assertThat(fedexRates.calculateRates(this.productIsFragileValue), is(100.2));
     }
 
     @Test
-    public void ifCalulateFedexOverWeigthValueIsCorrect(){
-        FedexRates fedexRates = new FedexRates();
+    public void calculateIfProductIsOverWeigth(){
         assertThat(fedexRates.calculateRates(this.productIsOverWeigth), is(200.2));
     }
 
