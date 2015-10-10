5470f6fa3ba9c5c391f53b2472881c032654b19e
Andre Almeida <andre.almeida@msn.com>
2015-10-10 19:38:15 -0300

    Insert method and remove init
    Insert setup method
    Remove Product init in method returnValueFreeProduct

--- test/calculate/shipping/rules/FreeRatesTest.java
+++ test/calculate/shipping/rules/FreeRatesTest.java
@@ -1,16 +1,28 @@
 package calculate.shipping.rules;
 
+import org.junit.Before;
 import product.Product;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class FreeRatesTest {
+    FreeRates freeRates;
+    private Product freeRatesProduct;
+
+    @Before
+    public void setUp() throws Exception {
+        freeRates = new FreeRates();
+
+    }
+
+    public FreeRatesTest() {
+        this.freeRatesProduct = new Product(2.0, false, false);
+    }
 
     @Test
-    public void ifCalculateFreeRateValueIsCorrect(){
-        Product productFreeValue = new Product(2.0, false, false);;
-        FreeRates freeRates = new FreeRates();
-        assertThat(freeRates.calculateRates(productFreeValue), is(2.0));
+    public void returnValueFreeProduct(){
+
+        assertThat(freeRates.calculateRates(freeRatesProduct), is(2.0));
     }
 }
